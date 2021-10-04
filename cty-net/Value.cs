3c80d49f464ce14429d564b549829ea8a9517d8a
Mark Pim <j.mark.pim@gmail.com>
2021-10-04 13:44:17 +0100

    Adding DynamicVal and UnknownVal

--- cty-net/Value.cs
+++ cty-net/Value.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace cty_net
 {
     public class Value
     {
+        private static readonly object Unknown = new object();
+        public static Value UnknownVal(IType type) => new Value(type, Unknown);
+        public static readonly Value DynamicVal = new Value(DynamicPseudoType.Instance, Unknown);
+        
         public Value(IType type, object? v)
         {
             Type = type;
@@ -10,5 +16,23 @@ namespace cty_net
 
         public IType Type { get; }
         public object? V { get; }
+    }
+
+    public class DynamicPseudoType : IType
+    {
+        public static readonly DynamicPseudoType Instance = new ();
+        private DynamicPseudoType()
+        {
+        }
+        public string GetFriendlyName(FriendlyTypeNameMode mode)
+        {
+            switch (mode)
+            {
+                case FriendlyTypeNameMode.ConstraintName:
+                    return "any type";
+                default:
+                    return "dynamic";
+            }
+        }
     }
 }
